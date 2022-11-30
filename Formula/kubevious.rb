92c7ac7426a936d3d352237abfa9c178a513c846
Kubevious CI/CD <cicd@kubevious.io>
2022-11-30 19:18:29 +0000

    Version Release v1.0.23

--- Formula/kubevious.rb
+++ Formula/kubevious.rb
@@ -3,8 +3,8 @@ require "language/node"
 class Kubevious < Formula
   desc "Detects and prevents Kubernetes misconfigurations and violations"
   homepage "https://github.com/kubevious/kubevious"
-  url "https://registry.npmjs.org/kubevious/-/kubevious-1.0.22.tgz"
-  sha256 "b1d64e21741d802c67c004531ea024ce19b18718aca97b445ed94dff049b144b"
+  url "https://registry.npmjs.org/kubevious/-/kubevious-1.0.23.tgz"
+  sha256 "f2120115644427a544883f89ec0d226100269c46954ef8b6fe750527fc04ad65"
   license "Apache-2.0"
 
   livecheck do
@@ -20,7 +20,87 @@ class Kubevious < Formula
   end
 
   test do
-    assert_match "v1.0.20",
+    assert_match "v1.0.23",
       shell_output("#{bin}/kubevious --version")
+
+    (testpath/"deployment.yml").write <<~EOF
+apiVersion: apps/v1
+kind: Deployment
+metadata:
+  name: nginx
+spec:
+  selector:
+    matchLabels:
+      app: nginx
+  replicas: 1
+  template:
+    metadata:
+      labels:
+        app: nginx
+    spec:
+      containers:
+      - name: nginx
+        image: nginx:1.14.2
+        ports:
+        - containerPort: 80
+EOF
+
+    assert_match "Lint Succeeded",
+      shell_output("#{bin}/kubevious lint #{testpath}/deployment.yml")
+
+    (testpath/"bad-deployment.yml").write <<~EOF
+apiVersion: apps/v1
+kind: BadDeployment
+metadata:
+  name: nginx
+spec:
+  selector:
+    matchLabels:
+      app: nginx
+  replicas: 1
+  template:
+    metadata:
+      labels:
+        app: nginx
+    spec:
+      containers:
+      - name: nginx
+        image: nginx:1.14.2
+        ports:
+        - containerPort: 80
+EOF
+
+    assert_match "Lint Failed",
+      shell_output("#{bin}/kubevious lint #{testpath}/bad-deployment.yml", 100)
+
+    assert_match "Guard Succeeded",
+      shell_output("#{bin}/kubevious guard #{testpath}/deployment.yml")
+
+    assert_match "Guard Failed",
+      shell_output("#{bin}/kubevious guard #{testpath}/bad-deployment.yml", 100)
+
+    (testpath/"service.yml").write <<~EOF
+apiVersion: v1
+kind: Service
+metadata:
+  labels:
+    app: nginx
+  name: nginx
+spec:
+  type: ClusterIP
+  ports:
+  - name: http
+    port: 80
+    targetPort: 8080
+  selector:
+    app: nginx
+EOF
+
+    assert_match "Guard Failed",
+      shell_output("#{bin}/kubevious guard #{testpath}/service.yml", 100)
+
+    assert_match "Guard Succeeded",
+      shell_output("#{bin}/kubevious guard #{testpath}/service.yml #{testpath}/deployment.yml")
+
   end
 end
