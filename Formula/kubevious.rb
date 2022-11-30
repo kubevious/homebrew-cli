6615a78ca8fef5dafd7cc7d65b78d8ad21520f4c
Kubevious CI/CD <cicd@kubevious.io>
2022-11-29 21:22:50 -0800

    Version Release v1.0.20

--- Formula/kubevious.rb
+++ Formula/kubevious.rb
@@ -4,8 +4,8 @@ class Kubevious < Formula
   desc "Detects and prevent Kubernetes misconfigurations and violations"
   homepage "https://github.com/kubevious/kubevious"
   url "https://github.com/kubevious/cli-releases/raw/main/v1.0.20/npm/kubevious.tgz"
-  license "Apache-2.0"
   sha256 "86942cf385472e2650b9032894643dd3cc1557575aa9336120ffec08ecf391f0"
+  license "Apache-2.0"
 
   depends_on "node@14"
 
